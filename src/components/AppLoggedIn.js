73071eb0b0de0e1bd87235a988c820eca4e7f572
Flavia Maia <flavialmaia@yahoo.com.br>
2021-07-08 09:51:37 -0300

    Atribuindo pastas e arrumando a Navbar - not yet finished!

--- src/components/AppLoggedIn.js
+++ src/components/AppLoggedIn.js
@@ -1,6 +1,7 @@
 import React from "react"
-import { Link } from 'react-router-dom'
-import SkillsList from "./SkillsList"
+import { Link, NavLink } from "react-router-dom";
+import SkillsList from "./forum/SkillsList"
+import Navbar from "./Navbar";
 
 import axios from "axios"
 
@@ -11,9 +12,6 @@ class AppLogedIn extends React.Component{
         points: ""
     }
 
-    // componentDidMount = () => {
-    //     this.setState({_id: this.props.match.params.id})
-    // }
     componentDidMount = async () => {
         try {
          // Atualizar o state com as informações prévias do user já salvas na API (id é o parâmetro de rota). 
@@ -32,34 +30,60 @@ class AppLogedIn extends React.Component{
     render(){
         return(
             <div>
+                <Navbar id={this.state_id} />
+{/* 
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <NavLink className="navbar-brand" to="/h">
+                        IronDummies
+                    </NavLink>
+
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                        
+                            <li className="nav-item dropdown">
+                                <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Track Your Progress
+                                </Link>
+                                <ul className="dropdownMenu" aria-labelledby="navbarDropdown">
+                                    <li><Link className="dropdownItem" to={`/updateskills/${this.state._id}`}>Module 1 - Update Your Skills</Link></li>
+                                    <li><Link className="dropdownItem" to={`/updateskills_m2/${this.state._id}`}>Module 2 - Update Your Skills</Link></li>
+                                    <li><Link className="dropdownItem" to={`/updateskills_m3/${this.state._id}`}>Module 3 - Update Your Skills</Link></li>
+                                    <li><hr className="dropdownDivider"/></li>
+                                    <li><Link className="dropdownItem" to={`/chart/${this.state._id}`}>Generate Your Report</Link></li>
+                                </ul>
+                            </li>
+                        </li>
+                        <Link className="nav-link" to="/forumbasics">
+                            Basics
+                        </Link>
+                        <Link className="nav-link" to="/forumfrontend">
+                            WebDev Front
+                        </Link>
+                        <Link className="nav-link" to="/forumbackend">
+                            WebDev Back
+                        </Link>
+                        <Link className="nav-link" to="/forumhavenoidea">
+                            Have no idea
+                        </Link>
+                        </ul>
+                    </div>
+                    </nav> */}
                 <h1>Componente AppLoggedIn</h1>
                 <h2>Welcome, {this.state.username}!</h2>
                 {/* <h2>You have {this.state.points} points!</h2> */}
-
-                <Link to={`/updateskills/${this.state._id}`}>
-                    <buton className="btn btn-primary">
-                        Update Your Skills M1!
-                    </buton>
-                </Link>
-
-                <Link to={`/updateskills_m2/${this.state._id}`}>
-                    <buton className="btn btn-primary">
-                        Update Your Skills M2!
-                    </buton>
-                </Link>
-
-                <Link to={`/updateskills_m3/${this.state._id}`}>
-                    <buton className="btn btn-primary">
-                        Update Your Skills M3!
-                    </buton>
-                </Link>
-
-                <Link to={`/chart/${this.state._id}`}>
-                    <buton className="btn btn-danger">
-                        Generate Skill Report!
-                    </buton>
-                </Link>
-
                 {/* Fórum com contribuições etc */}
                 < SkillsList/>
             </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,6 +20,39 @@ function Navbar() {
         <span className="navbar-toggler-icon"></span>
       </button>
 
+{/* LINKS PARA ATUALIZAR E GERAR GRÁFICO
+SUBSTITUIR props.id
+<div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <Link className="nav-link" to={`/chart/${this.state._id}`}>
+                            Track Your Progress <span class="sr-only">(current)</span>
+                            </Link>
+                            <Link to={`/updateskills/${this.state._id}`}>                   
+                            Update Your Skills M1!
+                            </Link>
+
+                            <Link to={`/updateskills_m2/${this.state._id}`}>
+                            Update Your Skills M2!
+                            </Link>
+
+                            <Link to={`/updateskills_m3/${this.state._id}`}>
+                            Update Your Skills M3!
+                            </Link>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                               Track Your Progress
+                            </Link>
+                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><Link className="dropdown-item" to={`/updateskills/${this.state._id}`}>Module 1 - Update Your Skills</Link></li>
+                                <li><Link className="dropdown-item" to={`/updateskills_m2/${this.state._id}`}>Module 2 - Update Your Skills</Link></li>
+                                <li><Link className="dropdown-item" to={`/updateskills_m3/${this.state._id}`}>Module 3 - Update Your Skills</Link></li>
+                                <li><hr className="dropdown-divider"/></li>
+                                <li><Link className="dropdown-item" to={`/chart/${this.state._id}`}>Generate Your Report</Link></li>
+                            </ul>
+                        </li> */}
+
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
